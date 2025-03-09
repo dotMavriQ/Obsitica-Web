d5647af1c392c178139ed3985543c15a655a5ecd
dotmavriq <dotmavriq@dotmavriq.life>
2025-03-09 11:36:44 +0000

    added improved style, responsivity etc

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gruvbox: {
+          dark0: '#282828',
+          dark1: '#3c3836',
+          dark2: '#504945',
+          dark3: '#665c54',
+          dark4: '#7c6f64',
+          light0: '#fbf1c7',
+          light1: '#ebdbb2',
+          light2: '#d5c4a1',
+          light3: '#bdae93',
+          light4: '#a89984',
+          red: '#fb4934',
+          green: '#b8bb26',
+          yellow: '#fabd2f',
+          blue: '#83a598',
+          purple: '#d3869b',
+          aqua: '#8ec07c',
+          orange: '#fe8019',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
